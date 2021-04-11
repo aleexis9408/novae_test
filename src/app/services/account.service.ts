bc35b24f27773f26bcb78a201ca2c4db3067ef48
mobileCrm <atello@opalo.com.co>
2021-04-11 09:47:13 -0500

    add services

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { requestGetAccount, responseGetAccount } from '../interfaces/novae.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  private basePath = 'v1/getAccount2';
+
+  constructor(private http: HttpClient) { }
+
+  getAccount(account: requestGetAccount): Observable<responseGetAccount> {
+    return this.http.post<responseGetAccount>(this.basePath, account);
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { requestLogin, responseLogin } from '../interfaces/novae.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private basePath = '/v1/login';
+  public isLogin = false;
+  constructor(private http: HttpClient) { }
+
+  login(loginObj: requestLogin): Observable<responseLogin> {
+    return this.http.post<responseLogin>(this.basePath, loginObj).pipe(
+      map((response: responseLogin) => {
+        const { code, message } = response.operationResult;
+        if (code !== "200") {
+          throw new Error(message);
+        }
+        this.isLogin = true;
+        sessionStorage.setItem('token', response.authenticationToken);
+        sessionStorage.setItem('email', loginObj.username);
+        sessionStorage.setItem('login', JSON.stringify(response));
+        return response;
+      }),
+
+      catchError((err) => { this.isLogin = false; return throwError(err); })
+    );
+  }
+
+
+}
